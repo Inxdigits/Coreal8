51183773b0694250a898e196d906732a4d783da7
Gbadamosi Olamide <lammiejay02@gmail.com>
2025-06-29 11:44:38 +0100

    created navbar and footer components

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Coreal8</h1>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
